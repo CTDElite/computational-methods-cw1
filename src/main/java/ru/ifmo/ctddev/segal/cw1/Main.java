0fc326d1ba24a27034cf216004ff1cee9efceff6
itegulov <ditegulov@gmail.com>
2016-05-24 00:08:22 +0300

    Initial commit

---
+++ src/main/java/ru/ifmo/ctddev/segal/cw1/Main.java
@@ -0,0 +1,11 @@
+package ru.ifmo.ctddev.segal.cw1;
+
+/**
+ * @author Daniyar Itegulov
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO: implement
+    }
+}
