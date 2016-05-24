79cf432cb77bfe19a71259bef6bf62ca27218190
alex-700 <alex_700_95@mail.ru>
2016-05-24 04:35:43 +0300

    add task3.Solver

--- src/main/java/ru/ifmo/ctddev/segal/cw1/task3/Solver.java
+++ src/main/java/ru/ifmo/ctddev/segal/cw1/task3/Solver.java
@@ -1,19 +1,23 @@
 package ru.ifmo.ctddev.segal.cw1.task3;
 
+import javafx.util.Pair;
 import ru.ifmo.ctddev.segal.cw1.Constants;
 import ru.ifmo.ctddev.segal.cw1.FunctionalMatrix;
 import ru.ifmo.ctddev.segal.cw1.FunctionalVector;
 import ru.ifmo.ctddev.segal.cw1.system_solvers.newton_method.NewtonMethod;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Solver {
     public static final int MAX_ITER = 1000;
     public static final double EPS = 1e-6;
-    public static final double[] start = new double[] {0, 0, 0, 0, 0};
+    public static final double[] start = new double[] {0, 0, 0, 0, 0, 0};
 
-    public double[] solve1(double T, double xg) {
+    public Pair<Map<Constants.Substance, Double>, Double> solve1(double T, double xg) {
         return solve(T, EPS, MAX_ITER, start, Arrays.asList(
                 30 * xg,
                 30 * (1 - xg),
@@ -24,7 +28,7 @@ public class Solver {
         ));
     }
 
-    public double[] solve2(double T, double xg) {
+    public Pair<Map<Constants.Substance, Double>, Double> solve2(double T, double xg) {
         return solve(T, EPS, MAX_ITER, start, Arrays.asList(
                 30 * xg,
                 30 * (1 - xg),
@@ -35,20 +39,26 @@ public class Solver {
         ));
     }
 
-    public double[] solve(double T, double EPS, int MAX_ITER, double[] start, List<Double> P) {
+    public Pair<Map<Constants.Substance, Double>, Double> solve(double T, double EPS, int MAX_ITER, double[] start, List<Double> P) {
         List<Double> K = Arrays.asList(
                 Constants.K(9, T),
                 Constants.K(10, T)
         );
-        List<Double> D = Arrays.asList(
-                Constants.Substance.AL_CL3.D(T),
-                Constants.Substance.GA_CL.D(T),
-                Constants.Substance.N_H3.D(T),
-                Constants.Substance.H_CL.D(T),
-                Constants.Substance.H2.D(T)
+        List<Constants.Substance> substances = Arrays.asList(
+                Constants.Substance.AL_CL3,
+                Constants.Substance.GA_CL,
+                Constants.Substance.N_H3,
+                Constants.Substance.H_CL,
+                Constants.Substance.H2
         );
+        List<Double> D = substances.stream().map(substance -> substance.D(T)).collect(Collectors.toList());
         FunctionalMatrix J = Jacobi.createTask3(K, D, P);
         FunctionalVector F = Task.task3(K, D, P);
-        return NewtonMethod.solve(start, J, F, EPS, MAX_ITER);
+        double[] ans = NewtonMethod.solve(start, J, F, EPS, MAX_ITER);
+        Map<Constants.Substance, Double> ret = new HashMap<>();
+        for (int i = 0; i < substances.size(); i++) {
+            ret.put(substances.get(i), ans[i]);
+        }
+        return new Pair<>(ret, ans[ans.length]);
     }
 }
